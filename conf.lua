7f40df117d910dc133e45046847111dfea0770b0
Simon Larsen <simonhffh@gmail.com>
2014-07-27 00:23:35 +0200

    Updated conf.lua for 0.9.1

--- conf.lua
+++ conf.lua
@@ -1,17 +1,18 @@
 function love.conf(t)
-    t.title = "In Your Face City Trains"
-    t.author = "Simon Larsen"
-	t.identity = "iyfct"
-	t.screen.width = 900
-	t.screen.height = 300
+    t.identity = "iyfct"
+    t.version = "0.9.1"
+    t.console = false
+
+    t.window.title = "In Your Face City Trains"
+    t.window.icon = nil
+    t.window.width = 900
+    t.window.height = 300
+    t.window.borderless = false
+    t.window.resizable = false
+    t.window.fullscreen = false
+    t.window.vsync = true
+
     t.modules.joystick = false
-    t.modules.audio = true
-    t.modules.keyboard = true
-    t.modules.event = true
-    t.modules.image = true
-    t.modules.graphics = true
-    t.modules.timer = true
     t.modules.mouse = false
-    t.modules.sound = true
     t.modules.physics = false
 end
